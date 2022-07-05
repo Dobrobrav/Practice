baa9882e17881f47e57eca9c7ead6aff0d64936c
user <kalyakulin.semen@gmail.com>
2022-07-05 23:52:27 +0700

    new task solved

--- practice/Stepik/Python ООП/3/test_poligon.py
+++ practice/Stepik/Python ООП/3/test_poligon.py
@@ -1,8 +1,11 @@
-from CODE_3_2_10 import *
+from CODE_3_3_4 import *
 
 
 if __name__ == '__main__':
-    render = RenderDigit()
-    input_dg = InputValues(render)(input)
-    print(input_dg())
+    words = WordString()
+    words.string = "Курс по Python ООП"
+    n = len(words)
+    first = "" if n == 0 else words(0)
+    print(words.string)
+    print(f"Число слов: {n}; первое слово: {first}")
 
