ba5e717ffd58123b14799cce000aeb1549108b65
user <kalyakulin.semen@gmail.com>
2022-07-15 17:32:13 +0700

    new task solved (some improvements needed)

---
+++ practice/Stepik/Python ООП/3/dir3_4_add_sub_mul_truediv/CODE_3_4_10.py
@@ -0,0 +1,56 @@
+from typing import List, Sequence, NamedTuple, Optional
+
+
+class Step(NamedTuple):
+    vertical: int
+    horizontal: int
+
+
+class Size(NamedTuple):
+    vertical: int
+    horizontal: int
+
+
+class MaxPooling:
+    step: Step
+    size: Size
+
+    def __init__(self, step=(2, 2), size=(2, 2)):
+        self.step = Step(*step)
+        self.size = Size(*size)
+
+    def __repr__(self):
+        return (f"step: {self.step.vertical}, {self.step.horizontal}; "
+                f"size: {self.size.vertical}, {self.step.horizontal}")
+
+    def __call__(self, matrix: Sequence[Sequence[float]]) -> List[List[float]]:
+        if (any(type(value) not in (int, float)
+                for row in matrix for value in row)
+                or any(len(row) != len(matrix[0]) for row in matrix)):
+            raise ValueError("Неверный формат для первого параметра matrix.")
+
+        max_pooling = [  # requires renaming because the name has been taken
+            [self._calc_area_max_value(matrix, i, j)
+             for j in range(0, len(matrix[0]), self.step.horizontal)
+             if self._calc_area_max_value(matrix, i, j) is not None]
+            for i in range(0, len(matrix), self.step.vertical)
+        ]
+        max_pooling = [row for row in max_pooling if row != []]
+        return max_pooling
+
+    def _calc_area_max_value(self, matrix: Sequence[Sequence[float]],
+                             i: int, j: int) -> Optional[float]:
+        sub_matrix = [  # may be unnecessary use of the comprehension
+            [value for value in row[j: j + self.size.horizontal]]
+            for row in matrix[i: i + self.size.vertical]
+        ]
+        area_of_sub_matrix = sum(len(row) for row in sub_matrix)
+
+        if area_of_sub_matrix < self.size.horizontal * self.size.vertical:
+            return None
+        max_value = max(
+            value
+            for row in sub_matrix
+            for value in row
+        )
+        return max_value
