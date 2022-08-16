7740637d2148df770bc34ecafea3e231af2e07a1
user <kalyakulin.semen@gmail.com>
2022-08-16 10:03:10 +0700

    did some refactoring

--- practice/Stepik/Python_OOP/chapt_3/dir3_9_iter_next/CODE_3_9_8.py
+++ practice/Stepik/Python_OOP/chapt_3/dir3_9_iter_next/CODE_3_9_8.py
@@ -41,34 +41,15 @@ class Stack:
 
     def __setitem__(self, key: int, value: str):
         self._check_index(key)
-        obj = StackObj(value)
+        node = StackObj(value)
 
-        if self._length == 1:
-            self.top = obj
-
-        elif self._length >= 2:
-            # if there are 2+ nodes, search for the node
-            current = self.top
-            for _ in range(key - 1):
-                current = current.next
-
-            if self._length - key == 1:  # if changing the latest added node
-                current.next = obj
-            else:  # if there are nodes after the one being searched for
-                if key == 0:
-                    obj.next = self.top.next
-                    self.top = obj
-                else:  # if index != 0
-                    next_obj = current.next
-                    current.next = obj
-                    obj.next = next_obj.next
+        if key == 0:  # and len(self) == 1
+            self._change_first_node(node)
+        elif len(self) >= 2:
+            self._change_node_in_long_stack(key, node)
 
     def __getitem__(self, item: int) -> str:
-        self._check_index(item)
-        current = self.top
-        for _ in range(item):
-            current = current.next
-        return current.data
+        return self._get_node(item).data
 
     def __iter__(self):
         obj = self.top
@@ -79,51 +60,90 @@ class Stack:
     def __len__(self):
         return self._length
 
-    def __repr__(self):  # Stack([obj1.data, obj2.data, objN.data])
-        return f"Stack({str(self._get_objects())})"
+    def __repr__(self):  # Stack([obj1.data, obj2.data, ..., objN.data])
+        return f"Stack({str(self._get_nodes())})"
 
-    def push_back(self, obj: StackObj) -> None:
-        """ Add StackObj instance to the end. """
+    def push_back(self, obj: StackObj):
+        """ Add StackObj to the end. """
         self._length += 1
-        if self.top is None:  # if stack is empty
-            self.top = obj
-            return
+        if not self.top:
+            return self._push_back_in_empty_stack(obj)
+        self._push_back_in_filled_stack(obj)
 
-        this_iteration_obj = self.top  # if stack is not empty
-        while this_iteration_obj.next is not None:
-            this_iteration_obj = this_iteration_obj.next
-        this_iteration_obj.next = obj
-
-    def push_front(self, obj: StackObj):
-        """ Add stackObj instance to the beginning. """
-        if self.top is None:
+    def push_front(self, obj: StackObj):  # need to decompose it!
+        """ Add StackObj to the beginning. """
+        if not self.top:  # empty stack
             self.top = obj
         else:
-            next_obj = self.top
-            obj.next = next_obj
-            self.top = obj
+            self._push_front_in_filled_stack(obj)
         self._length += 1
 
     def pop(self) -> Optional[StackObj]:
-        """ Delete and return the latest added StackObj. """
-        if self.top is None:  # if the stack is empty
+        if not self.top:  # empty stack
             return None
 
         self._length -= 1
-        current_obj = self.top
-        if current_obj.next is None:  # if there's only 1 node in the stack
-            popped_obj = self.top
-            self.top = None
-            return popped_obj
+        if self.top.next is None:  # 1 node in the stack
+            return self._pop_in_one_node_stack()
+        return self._pop_in_long_stack()  # 2+ nodes in the stack
 
-        while current_obj.next.next is not None:  # if there're 2+ nodes in the stack
-            current_obj = current_obj.next
-        popped_obj = current_obj.next  # type: ignore
-        current_obj.next = None
-        return popped_obj
+    def _push_front_in_filled_stack(self, obj: StackObj):
+        next_obj = self.top
+        obj.next = next_obj
+        self.top = obj
 
-    def _get_objects(self) -> List[StackObj]:
-        """ Return all objects of the stack. """
+    def _push_back_in_empty_stack(self, obj: StackObj):
+        if self.top is None:  # empty stack
+            self.top = obj
+
+    def _push_back_in_filled_stack(self, obj: StackObj):
+        current_node = self.top  # not empty stack
+        while current_node.next is not None:
+            current_node = current_node.next
+        current_node.next = obj
+
+    def _pop_in_one_node_stack(self) -> StackObj:
+        popped_node = self.top
+        self.top = None
+        return popped_node
+
+    def _pop_in_long_stack(self) -> StackObj:
+        current_node = self.top
+        while current_node.next.next is not None:
+            current_node = current_node.next
+        popped_node = current_node.next  # type: ignore
+        current_node.next = None
+        return popped_node
+
+    def _change_first_node(self, node: StackObj):
+        node.next = self.top.next
+        self.top = node
+
+    def _change_node_in_long_stack(self, key: int, node: StackObj):
+        if len(self) - key == 1:  # changing the last node
+            self._change_last_node(node)
+        else:  # changing middle node
+            self._change_middle_node(key, node)
+
+    def _get_node(self, key: int):
+        self._check_index(key)
+        node = self.top
+        for _ in range(key - 1):
+            node = node.next
+        return node
+
+    def _change_last_node(self, node: StackObj):
+        current = self._get_node(len(self) - 2)
+        current.next = node
+
+    def _change_middle_node(self, key: int, node: StackObj):
+        current = self._get_node(key)
+        next_obj = current.next
+        current.next = node
+        node.next = next_obj.next
+
+    def _get_nodes(self) -> List[StackObj]:
+        """ Return all nodes of the stack. """
         if self.top is None:
             return []
 
@@ -135,5 +155,5 @@ class Stack:
         return objects
 
     def _check_index(self, index: int):
-        if type(index) is not int or self._length <= index:
+        if type(index) is not int or len(self) <= index:
             raise IndexError('неверный индекс')
