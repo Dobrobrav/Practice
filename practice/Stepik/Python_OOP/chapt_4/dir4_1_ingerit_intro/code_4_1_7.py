bd1ceb2267131d143ae1318ca10d37584d905b6f
user <kalyakulin.semen@gmail.com>
2022-08-20 19:21:26 +0700

    solved new task

---
+++ practice/Stepik/Python_OOP/chapt_4/dir4_1_ingerit_intro/code_4_1_7.py
@@ -0,0 +1,23 @@
+from typing import Any, Dict
+
+
+class MetaSingleton(type):
+    _instances: Dict['MetaSingleton', Any] = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(MetaSingleton, cls)\
+                .__call__(*args, **kwargs)
+
+        return cls._instances[cls]
+
+
+class Singleton(metaclass=MetaSingleton):
+    pass
+
+
+class Game(Singleton):
+    name: str
+
+    def __init__(self, name: str):
+        self.name = name
