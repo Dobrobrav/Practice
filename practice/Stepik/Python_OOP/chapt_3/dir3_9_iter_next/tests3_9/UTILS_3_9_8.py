5c0963220510e74f7d1e93efe931dd0c78ae0a08
user <kalyakulin.semen@gmail.com>
2022-08-25 10:39:25 +0700

    some refactoring

--- practice/Stepik/Python_OOP/chapt_3/dir3_9_iter_next/tests3_9/UTILS_3_9_8.py
+++ practice/Stepik/Python_OOP/chapt_3/dir3_9_iter_next/tests3_9/UTILS_3_9_8.py
@@ -2,8 +2,8 @@ from practice.Stepik.Python_OOP.chapt_3.dir3_9_iter_next \
     .CODE_3_9_8 import *
 
 
-def get_nodes(stack) -> List[StackObj]:
-    """ Return all nodes of the stack. """
+def get_nodes(stack: Stack) -> List[StackObj]:
+    """ Return all nodes of stack. """
     if stack.top is None:
         return []
 
@@ -36,19 +36,6 @@ def push_back(stack: Stack, obj: StackObj):
     if not stack.top:
         return _push_back_in_empty_stack(stack, obj)
     _push_back_in_filled_stack(stack, obj)  # 1+ nodes in stack
-
-
-def get_nodes(stack: Stack) -> List[StackObj]:
-    """ Return all nodes of the stack. """
-    if stack.top is None:
-        return []
-
-    objects = [stack.top]
-    current_obj = stack.top
-    while current_obj.next is not None:
-        objects.append(current_obj.next)
-        current_obj = current_obj.next
-    return objects
 
 
 def _push_back_in_empty_stack(stack: Stack, obj: StackObj):
