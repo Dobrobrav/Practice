86d7c617b886085380a0a489c74e7a03ad1c79a5
semen <kalyakulin.semen@gmail.com>
2023-02-25 13:12:10 +0700

    moved files

--- practice/Stepik/Python_OOP/c4_inherit_polimorph/d3_super/code_4_3_8.py
+++
@@ -1,6 +0,0 @@
-class SoftList(list):
-    def __getitem__(self, item):
-        try:
-            return super(SoftList, self).__getitem__(item)
-        except IndexError:
-            return False
